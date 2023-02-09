f5152c5b90304496174d98ddacd213052581e0c7
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-02-10 02:19:04 +0300

    Done with while loop assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    i = number
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+    i = seconds
+    while i > 0
+        sleep(1)
+        i -= 1
+    end
 end
